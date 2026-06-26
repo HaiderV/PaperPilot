6f34c8140dac9a9eb19aef90a48be8d231008ab9
HaiderV <haidervadgamwala3@gmail.com>
2026-06-26 16:48:59 +0530

    Get added in ocr.routes.js

--- backend/routes/ocr.routes.js
+++ backend/routes/ocr.routes.js
@@ -4,6 +4,12 @@ import { uploadPDF } from "../controllers/ocr.controller.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.json({
+        message: "OCR Routes Working"
+    })
+})
+
 router.post(
     "/upload",
     upload.single("file"),
