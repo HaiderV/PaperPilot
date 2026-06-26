4cebcdd3541f29e27d47aa5309696c5f80930b01
HaiderV <haidervadgamwala3@gmail.com>
2026-06-27 01:52:07 +0530

    Cloudinary intigration

---
+++ backend/services/cloudinary.service.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadPDFToCloudinary = async (filePath) => {
+    try {
+
+        const result = await cloudinary.uploader.upload(filePath, {
+            resource_type: "raw",
+            folder: "paperpilot/ocr",
+            use_filename: true,
+            unique_filename: true,
+        });
+
+        return result;
+
+    } catch (error) {
+
+        throw new Error(`Cloudinary Upload Failed: ${error.message}`);
+
+    }
+};
