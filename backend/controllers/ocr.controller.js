4ceae157d295878e85131768aa0ae561bce0fbbb
HaiderV <haidervadgamwala3@gmail.com>
2026-06-27 12:50:33 +0530

    Cloudinary test controller updated

--- backend/controllers/ocr.controller.js
+++ backend/controllers/ocr.controller.js
@@ -2,13 +2,11 @@ import { runOCR } from "../services/ocr.service.js";
 import { uploadPDFToCloudinary } from "../services/cloudinary.service.js";
 
 export const uploadPDF = async (req, res) => {
-
     try {
-
         if (!req.file) {
             return res.status(400).json({
                 success: false,
-                message: "No PDF uploaded."
+                message: "No PDF uploaded.",
             });
         }
 
@@ -16,20 +14,18 @@ export const uploadPDF = async (req, res) => {
 
         const cloudinaryFile = await uploadPDFToCloudinary(outputPath);
 
-        return res.json({
+        return res.status(200).json({
             success: true,
+            message: "OCR completed successfully.",
             cloudinaryFile,
         });
 
     } catch (error) {
-
         console.error(error);
 
         return res.status(500).json({
             success: false,
-            message: error.message
+            message: error.message,
         });
-
     }
-
 };
